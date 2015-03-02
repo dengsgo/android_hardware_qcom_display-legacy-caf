774b9c9f77c430b30372664e8d56955004eb49ff
BambooIV <ningdisheng@gmail.com>
2015-03-02 15:35:43 +0800

    get rid of HAL pixelformats 5551 and 4444 former commit c7911e343ecfeec3fd9c3a9a102a5ee8d58b15b9

--- libqdutils/qcom_ui.cpp
+++ libqdutils/qcom_ui.cpp
@@ -548,12 +548,6 @@ inline void getHalPixelFormatStr(int format, char pixelformatstr[])
         case HAL_PIXEL_FORMAT_BGRA_8888:
             strcpy(pixelformatstr, "BGRA_8888");
             break;
-        case HAL_PIXEL_FORMAT_RGBA_5551:
-            strcpy(pixelformatstr, "RGBA_5551");
-            break;
-        case HAL_PIXEL_FORMAT_RGBA_4444:
-            strcpy(pixelformatstr, "RGBA_4444");
-            break;
         case HAL_PIXEL_FORMAT_YV12:
             strcpy(pixelformatstr, "YV12");
             break;
@@ -680,8 +674,6 @@ void dumpLayer(int moduleCompositionType, int listFlags, size_t layerIndex,
                 tempSkBmpConfig = SkBitmap::kARGB_8888_Config;
                 break;
             case HAL_PIXEL_FORMAT_RGB_565:
-            case HAL_PIXEL_FORMAT_RGBA_5551:
-            case HAL_PIXEL_FORMAT_RGBA_4444:
                 tempSkBmpConfig = SkBitmap::kRGB_565_Config;
                 break;
             case HAL_PIXEL_FORMAT_RGB_888:
